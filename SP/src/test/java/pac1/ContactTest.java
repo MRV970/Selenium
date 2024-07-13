20668d14876655d9222076dbb37087b000439d45
MRV970 <midderaghavamma@gmail.com>
2024-07-13 10:44:06 +0530

    print st

--- SP/src/test/java/pac1/ContactTest.java
+++ SP/src/test/java/pac1/ContactTest.java
@@ -5,14 +5,8 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test
 	public void createcontacttest() {
-		String Url=System.getProperty("url");
-		String Browser=System.getProperty("browser");
-		String Username=System.getProperty("username");
-		String Password=System.getProperty("password");
-		System.out.println(Url);
-		System.out.println(Browser);
-		System.out.println(Username);
-		System.out.println(Password);
+		System.out.println("Test1");
+		System.out.println("Test2");
 	}
 	@Test
 	public void modifycontacttest() {
